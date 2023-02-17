38a3c5e1c66ec20a8f31d9b55a3a706d1c5fa554
viviviantung <90019550+viviviantung@users.noreply.github.com>
2023-02-16 22:29:51 -0500

    only have 3 balls

--- AR_BaseGameDemo/Assets/Scripts/ProjectileLauncher.cs
+++ AR_BaseGameDemo/Assets/Scripts/ProjectileLauncher.cs
@@ -17,6 +17,7 @@
 
         public GameObject goalPrefab;
         public GameObject goal1 { get; private set; }
+        public int ballSupply = 3;
 
 
         protected override void OnPressBegan(Vector3 position)
@@ -35,16 +36,23 @@
             // Cast a ray from the touch point to the world. We use the camera position as the origin and the ray direction as the
             // velocity direction.
             var ray = this.GetComponent<Camera>().ScreenPointToRay(position);
-            var projectile = PhotonNetwork.Instantiate(this.projectilePrefab.name, ray.origin, Quaternion.identity, data: initialData);
+            if (ballSupply > 0) {
+                var projectile = PhotonNetwork.Instantiate(this.projectilePrefab.name, ray.origin, Quaternion.identity, data: initialData);
+                var rigidbody = projectile.GetComponent<Rigidbody>();
+                rigidbody.isKinematic = false;
+                rigidbody.velocity = ray.direction * initialSpeed;
+                ballSupply--;
+            }
+            //var projectile = PhotonNetwork.Instantiate(this.projectilePrefab.name, ray.origin, Quaternion.identity, data: initialData);
             //this.goal1 = PhotonNetwork.Instantiate(this.goalPrefab.name, new Vector3(0.5f, 0, 0), Quaternion.identity, data: initialData);
 
             // By default, the projectile is kinematic in the prefab. This is because it should not be affected by physics
             // on clients other than the one owning it. Hence we disable kinematic mode and let the physics engine take over here.
             // It might make sense to have all game physics run on the server for a more complex scenario. You could transfer
             // ownership here to the server.
-            var rigidbody = projectile.GetComponent<Rigidbody>();
-            rigidbody.isKinematic = false;
-            rigidbody.velocity = ray.direction * initialSpeed;
+            // var rigidbody = projectile.GetComponent<Rigidbody>();
+            // rigidbody.isKinematic = false;
+            // rigidbody.velocity = ray.direction * initialSpeed;
         }
     }
 }
